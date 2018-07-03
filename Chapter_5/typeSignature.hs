b1ec32a7edbb32c17f861864961641cf6340a2ff
Stefan Dourado <dearz@hotmail.de>
2018-07-03 23:59:01 +0100

    Slight changes to typeSignature.hs

--- Chapter_5/typeSignature.hs
+++ Chapter_5/typeSignature.hs
@@ -6,8 +6,8 @@ module TypeSignature where
 functionH :: [a] -> a
 functionH (x:_) = x
 
--- 2 Wrong answer. To be comparable, they need to be the same type,
---   so (Ord a) => a -> a -> Bool is sufficient.
+-- 2 Slightly wrong. To be comparable, they need to be the same type,
+--   so (Ord a) => a -> a -> Bool is sufficient enough.
 functionC :: (Ord a, Ord b) => a -> b -> Bool
 functionC x y = if (x > y) then True else False
 
