aca9efc6fcd0f6ac4e8d9c656dbb208ac7dcae5e
Stefan Dourado <dearz@hotmail.de>
2018-07-05 22:46:49 +0100

    More instances of Eq.

--- Chapter_6/eqInstances.hs
+++ Chapter_6/eqInstances.hs
@@ -4,6 +4,7 @@ module EqInstances where
 
 data TisAnInteger =
   TisAn Integer
+  deriving Show
 
 instance Eq TisAnInteger where
   (==) (TisAn int) (TisAn int') = int == int'
@@ -11,6 +12,7 @@ instance Eq TisAnInteger where
 
 data TwoIntegers = 
   Two Integer Integer
+  deriving Show
 
 instance Eq TwoIntegers where
   (==) (Two intOne intTwo) 
@@ -21,6 +23,7 @@ instance Eq TwoIntegers where
 data StringOrInt = 
     TisAnInt   Int
   | TisAString String
+    deriving Show
 
 instance Eq StringOrInt where
   (==) (TisAnInt int) 
@@ -30,4 +33,46 @@ instance Eq StringOrInt where
   (==) (TisAString string) 
        (TisAString string') = 
     string == string'
+
+
+data Pair a = 
+  Pair a a 
+  deriving Show
+
+instance Eq a => Eq (Pair a) where
+  (==) (Pair vOne vTwo) 
+       (Pair vOne' vTwo') = 
+    vOne == vOne' && vTwo == vTwo'
+
+
+data Tuple a b = 
+  Tuple a b 
+  deriving Show
     
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple vOne vTwo)
+       (Tuple vOne' vTwo') = 
+    vOne == vOne' && vTwo == vTwo' 
+
+
+data Which a = 
+    ThisOne a
+  | ThatOne a
+    deriving Show
+instance Eq a => Eq (Which a) where
+  (==) (ThisOne v)(ThisOne v') = 
+    v == v'
+  (==) (ThatOne v)(ThatOne v') = 
+    v == v'
+
+
+data EitherOr a b = 
+    Hello a
+  | Goodbye b
+    deriving Show
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  (==) (Hello v) (Hello v') = 
+    v == v'
+  (==) (Goodbye v)(Goodbye v') = 
+    v == v'
